b18792d162054cad6cee39f75356cb8cb2bbe7a0
Salman-S7 <sammyzed1505@gmail.com>
2023-09-16 21:08:59 +0530

    the messege .. data is not inserted into tables why ..?

--- learn-jpa-and-hibernate/src/main/java/LearnJpaHibernate/LearnCoomadLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/LearnJpaHibernate/LearnCoomadLineRunner.java
@@ -4,6 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/*
+ * Everything is correct but still i am not able to put data into database ..
+ * why is this is happening i don't know 
+ * i'll try to find solution for it..
+ *  
+ * */
 @Component
 public class LearnCoomadLineRunner implements CommandLineRunner {
 	@Autowired
