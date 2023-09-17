5663edba25c979aed3c2136eb6c1933c1ad9875d
Salman-S7 <sammyzed1505@gmail.com>
2023-09-17 14:17:15 +0530

    performed crude operations using spring jdbc

--- demo/src/main/java/com/example/demo/CommandLineRunnerClassSpringJdbc.java
+++ demo/src/main/java/com/example/demo/CommandLineRunnerClassSpringJdbc.java
@@ -8,8 +8,16 @@ import org.springframework.stereotype.Component;
 public class CommandLineRunnerClassSpringJdbc implements CommandLineRunner {
     @Autowired
     private JdbcRepo jdbcRepo;
+
     @Override
-    public void run(String... args) throws Exception {
-        jdbcRepo.insert();
+    public void run(String... args) {
+        jdbcRepo.insert(new Course(1, "learn java", "salman"));
+        jdbcRepo.insert(new Course(2, "learn spring", "salman1"));
+        jdbcRepo.insert(new Course(3, "learn spring boot", "salman2"));
+        jdbcRepo.delete(1);
+        jdbcRepo.updateById(2);
+        System.out.println(jdbcRepo.getById(3));
+        System.out.println(jdbcRepo.getById(2));
+
     }
 }
--- demo/src/main/java/com/example/demo/JdbcRepo.java
+++ demo/src/main/java/com/example/demo/JdbcRepo.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,13 +9,44 @@ import org.springframework.stereotype.Repository;
 public class JdbcRepo {
     private static String INSERT_QUERY = """
             INSERT INTO courses(id,name,author) 
-            values(1,'learn spring boot', 'salman');
+            values(?,?,?);
+            """;
+    private static String DELETE_QUERY = """
+            delete from courses where id = ?;
+            """;
+    private static String GET_QUERY = """
+            select * from courses where id = ?;
+            """;
+    private static String UPDATE_QUERY = """
+            update courses set name = ? , author = ? where id = ?;
             """;
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void insert(){
-        jdbcTemplate.update(INSERT_QUERY);
+    public void insert(Course course) {
+        jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course getById(int id) {
+        Course course = null;
+        try {
+            course = jdbcTemplate.queryForObject(GET_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return course;
+
+    }
+
+    /**
+     * @param id
+     */
+    public void updateById(int id) {
+        jdbcTemplate.update(UPDATE_QUERY, "learn ai and ml", "salman shaikh", id);
     }
 
 
