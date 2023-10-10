f583e6b60c2e659973fab7d987afb03b83749aa8
Salman-S7 <sammyzed1505@gmail.com>
2023-10-10 10:10:09 +0530

    tutor can see courses

--- learning-thymeleaf/src/main/java/com/thymeleaf/learningthymeleaf/thymeleafdemo/controller/TutorController.java
+++ learning-thymeleaf/src/main/java/com/thymeleaf/learningthymeleaf/thymeleafdemo/controller/TutorController.java
@@ -4,13 +4,11 @@ import com.thymeleaf.learningthymeleaf.thymeleafdemo.dao.CourseRepository;
 import com.thymeleaf.learningthymeleaf.thymeleafdemo.dao.TutorRepository;
 import com.thymeleaf.learningthymeleaf.thymeleafdemo.model.Course;
 import com.thymeleaf.learningthymeleaf.thymeleafdemo.model.Tutor;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import java.util.Set;
 
 @Controller
 public class TutorController {
@@ -25,10 +23,25 @@ public class TutorController {
     }
     @PostMapping("/tutor")
     public String addCourse(@ModelAttribute("Course") Course course, @RequestParam("tutorId") Long tutorId) {
-        System.out.println(tutorId);
         Tutor tutor = tutorRepository.findById(tutorId).get();
         course.setTutor(tutor);
         courseRepository.save(course);
         return "dashboard";
+    }
+
+    @GetMapping("/mycourses")
+    public String myCourses(@RequestParam long tutorId,ModelMap modelMap){
+        Tutor tutor = tutorRepository.findById(tutorId).get();
+        Set<Course> courses = tutor.getCourseSet();
+        modelMap.addAttribute("courses",courses);
+        return "mycourses";
+    }
+
+    //still need to update the course
+    @GetMapping("update")
+    public String updateCourse(@RequestParam long courseId, ModelMap model){
+        Course course = courseRepository.findById(courseId).get();
+        model.addAttribute("Course",course);
+        return "tutor";
     }
 }
